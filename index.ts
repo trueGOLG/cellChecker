e67bd7bd03861dcde8d79292d16a04f277146dfa
GeorgiiMB <georgii@nowhere.co.jp>
2023-12-18 22:49:11 +0900

    completed calculation formula

--- index.ts
+++ index.ts
@@ -6,35 +6,50 @@ interface DataPoint {
     y: number
 }
 
-const cw = 6
-const ch = 4
-const max_X = 6
-const max_Y = 4
-const sw = 0.5 * (0.5 * max_X)
-const sh = 0.5 * (0.5 * max_Y)
+const out = (val: string | number) => console.log(val)
+const intDiv = (num: number, div: number) => Math.floor(num / div)
 
-const intDiv = (num, div) => Math.floor(num / div)
+
+const cellWidth = 6    
+const cellHeight = 4
+
+const fieldSize_X = 12
+const fieldSize_Y = 8
+
+const max_X = 0.5 * fieldSize_X
+const max_Y = 0.5 * fieldSize_Y
+
+const rx = max_X / (0.5 * cellWidth)
+const ry = max_Y / (0.5 * cellHeight)
+
 const getCell = (dp: DataPoint) => {
-    const xn = intDiv(dp.x + sw, 2)
-    const yn = intDiv(dp.y + sh, 2)
-    const cellN = sw * (sh - yn) - sw + xn + 1
+    const xn = intDiv(dp.x + max_X, rx)
+    const yn = intDiv(dp.y + max_Y, ry)
+    const cellN = cellWidth * (cellHeight - yn) - cellWidth + xn + 1
 
+    // console.log(`${dp.name} - x': ${xn} ; y: ${yn}  --- ${cellN}`)
     console.log(`${dp.name} - x': ${xn} ; y: ${yn}  --- ${cellN}`)
+    
 }
 
 const dataPoints: DataPoint[] = [
-    { name: 'a', x: -3, y: 3 },
-    { name: 'b', x: -3, y: 2.5 },
-    { name: 'c', x: -1, y: 1 },
-    { name: 'd', x: 0.8, y: -1 },
-    { name: 'e', x: 4.2, y: -3.5 },
-    { name: 'f', x: 5, y: 3.2 },
-    { name: 'g', x: 4.2, y: 3.5 },
-    { name: 'h', x: -5, y: -2.5 },
-    { name: 'i', x: -3.1, y: -2.5 },
-    { name: 'x', x: 0, y: 0 },
-    { name: 'y', x: 6, y: 0 },
-    { name: 'z', x: 6, y: -4 },
+    // { name: 'a', x: -3, y: 3 },
+    // { name: 'b', x: -3, y: 2.5 },
+    // { name: 'c', x: -1, y: 1 },
+    // { name: 'd', x: 0.8, y: -1 },
+    // { name: 'e', x: 4.2, y: -3.5 },
+    // { name: 'f', x: 5, y: 3.2 },
+    // { name: 'g', x: 4.2, y: 3.5 },
+    // { name: 'h', x: -5, y: -2.5 },
+    // { name: 'i', x: -3.1, y: -2.5 },
+    // { name: 'x', x: 0, y: 0 },
+    // { name: 'y', x: 6, y: -4 },
+    { name: 'x1', x: -6, y: -4 },
+    { name: 'x2', x: -6, y: -4 },
+    { name: 'x3', x: -6, y: 0 },
+    { name: 'x4', x: -6, y: 4 },
+    { name: 'x5', x: -2, y: 0 },
+    { name: 'x6', x: -4, y: 2 },
 ]
 
 
